922a053e15b63678ef0fa17116a738e93f8b7060
Thinh Truong Cong <truongcongthinh.contact@gmail.com>
2023-08-30 16:26:47 +0700

    Remove comments

--- src/components/bestseller/index.js
+++ src/components/bestseller/index.js
@@ -15,44 +15,6 @@ const BestSeller = () => {
           {sellerData.map((slide, i) => {
             return <SlideItem key={i} slide={slide} index={i} />;
           })}
-          {/* <div className='carousel-item'>
-            <div className='bl_bestSeller_item_wrap'>
-              <div>
-                <img src='assets/images/bseller-item1.png' alt='' />
-                <h3 className='hp_fz-16_fw-400'>Euphoria Crop Blouse</h3>
-                <h4 className='hp_fz-14_fw-400'>Rp. 125.000</h4>
-              </div>
-              <div>
-                <img src='assets/images/bseller-item2.png' alt='' />
-                <h3 className='hp_fz-16_fw-400'>Venus Halter Dress</h3>
-                <h4 className='hp_fz-14_fw-400'>Rp. 180.000</h4>
-              </div>
-              <div>
-                <img src='assets/images/bseller-item3.png' alt='' />
-                <h3 className='hp_fz-16_fw-400'>Snap Pure Blouse</h3>
-                <h4 className='hp_fz-14_fw-400'>Rp. 129.000</h4>
-              </div>
-            </div>
-          </div>
-          <div className='carousel-item'>
-            <div className='bl_bestSeller_item_wrap'>
-              <div>
-                <img src='assets/images/bseller-item4.png' alt='' />
-                <h3 className='hp_fz-16_fw-400'>West Bootscut Denim</h3>
-                <h4 className='hp_fz-14_fw-400'>Rp. 229.000</h4>
-              </div>
-              <div>
-                <img src='assets/images/bseller-item5.png' alt='' />
-                <h3 className='hp_fz-16_fw-400'>Ribbon Check Long</h3>
-                <h4 className='hp_fz-14_fw-400'>Rp. 175.000</h4>
-              </div>
-              <div>
-                <img src='assets/images/bseller-item6.png' alt='' />
-                <h3 className='hp_fz-16_fw-400'>Shouldcut Knit</h3>
-                <h4 className='hp_fz-14_fw-400'>Rp. 149.000</h4>
-              </div>
-            </div>
-          </div> */}
         </div>
         <a
           className='carousel-control-prev'
